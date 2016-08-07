9e1633bd6a5ea6f78eb83e695c7b2a00bcc1cbc6
PritamPaul <pritam@teampumpkin.com>
2016-08-07 16:21:40 +0530

    1st commit

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -20,7 +20,7 @@ $(function () {
     //	Checkbox filtering
 
     var checkboxes = $('.all-products input[type=checkbox]');
-    var range = $('');
+    var search = $('.form-wrapper input[type=submit]');
 
     checkboxes.click(function () {
 
@@ -28,10 +28,10 @@ $(function () {
             specName = that.attr('name');
 
         // When a checkbox is checked we need to write that in the filters object;
-        if(that.is(":checked")) {
+        if (that.is(":checked")) {
 
             // If the filter for this specification isn't created yet - do it.
-            if(!(filters[specName] && filters[specName].length)){
+            if (!(filters[specName] && filters[specName].length)) {
                 filters[specName] = [];
             }
 
@@ -44,9 +44,9 @@ $(function () {
         }
 
         // When a checkbox is unchecked we need to remove its value from the filters object.
-        if(!that.is(":checked")) {
+        if (!that.is(":checked")) {
 
-            if(filters[specName] && filters[specName].length && (filters[specName].indexOf(that.val()) != -1)){
+            if (filters[specName] && filters[specName].length && (filters[specName].indexOf(that.val()) != -1)) {
 
                 // Find the checkbox value in the corresponding array inside the filters object.
                 var index = filters[specName].indexOf(that.val());
@@ -56,7 +56,54 @@ $(function () {
 
                 // If it was the last remaining value for this specification,
                 // delete the whole array.
-                if(!filters[specName].length){
+                if (!filters[specName].length) {
+                    delete filters[specName];
+                }
+
+            }
+
+            // Change the url hash;
+            createQueryHash(filters);
+        }
+    });
+
+
+    search.click(function () {
+
+        var that = $(this),
+            specName = that.attr('name');
+        var x=document.getElementById("search").value;
+
+        // When a checkbox is checked we need to write that in the filters object;
+        if (x!='') {
+
+            // If the filter for this specification isn't created yet - do it.
+            if (!(filters[specName] && filters[specName].length)) {
+                filters[specName] = [];
+            }
+
+            //	Push values into the chosen filter array
+            filters[specName].push(that.val());
+
+            // Change the url hash;
+            createQueryHash(filters);
+
+        }
+
+        // When a checkbox is unchecked we need to remove its value from the filters object.
+        if (!that.is(":checked")) {
+
+            if (filters[specName] && filters[specName].length && (filters[specName].indexOf(that.val()) != -1)) {
+
+                // Find the checkbox value in the corresponding array inside the filters object.
+                var index = filters[specName].indexOf(that.val());
+
+                // Remove it.
+                filters[specName].splice(index, 1);
+
+                // If it was the last remaining value for this specification,
+                // delete the whole array.
+                if (!filters[specName].length) {
                     delete filters[specName];
                 }
 
@@ -98,7 +145,7 @@ $(function () {
     // These are called on page load
 
     // Get data about our products from products.json.
-    $.getJSON( "products.json", function( data ) {
+    $.getJSON("products.json", function (data) {
 
         // Write the data into our global variable.
         products = data;
@@ -113,7 +160,7 @@ $(function () {
 
     // An event handler with calls the render function on every hashchange.
     // The render function will show the appropriate content of out page.
-    $(window).on('hashchange', function(){
+    $(window).on('hashchange', function () {
         render(decodeURI(window.location.hash));
     });
 
@@ -129,20 +176,20 @@ $(function () {
         $('.main-content .page').removeClass('visible');
 
 
-        var	map = {
+        var map = {
 
             // The "Homepage".
-            '': function() {
+            '': function () {
 
                 // Clear the filters object, uncheck all checkboxes, show all the products
                 filters = {};
-                checkboxes.prop('checked',false);
+                checkboxes.prop('checked', false);
 
                 renderProductsPage(products);
             },
 
             // Single Products page.
-            '#product': function() {
+            '#product': function () {
 
                 // Get the index of which product we want to show and call the appropriate function.
                 var index = url.split('#product/')[1].trim();
@@ -151,7 +198,7 @@ $(function () {
             },
 
             // Page with filtered products
-            '#filter': function() {
+            '#filter': function () {
 
                 // Grab the string after the '#filter/' keyword. Call the filtering function.
                 url = url.split('#filter/')[1].trim();
@@ -161,7 +208,7 @@ $(function () {
                     filters = JSON.parse(url);
                 }
                     // If it isn't a valid json, go back to homepage ( the rest of the code won't be executed ).
-                catch(err) {
+                catch (err) {
                     window.location.hash = '#';
                     return;
                 }
@@ -172,7 +219,7 @@ $(function () {
         };
 
         // Execute the needed function depending on the url keyword (stored in temp).
-        if(map[temp]){
+        if (map[temp]) {
             map[temp]();
         }
         // If the keyword isn't listed in the above - render the error page.
@@ -186,7 +233,7 @@ $(function () {
     // This function is called only once - on page load.
     // It fills up the products list via a handlebars template.
     // It recieves one parameter - the data we took from products.json.
-    function generateAllProductsHTML(data){
+    function generateAllProductsHTML(data) {
 
         var list = $('.all-products .products-list');
 
@@ -209,7 +256,7 @@ $(function () {
     }
 
     // This function receives an object containing all the product we want to show.
-    function renderProductsPage(data){
+    function renderProductsPage(data) {
 
         var page = $('.all-products'),
             allProducts = $('.all-products .products-list > li');
@@ -224,7 +271,7 @@ $(function () {
             var that = $(this);
 
             data.forEach(function (item) {
-                if(that.data('index') == item.id){
+                if (that.data('index') == item.id) {
                     that.removeClass('hidden');
                 }
             });
@@ -239,15 +286,15 @@ $(function () {
 
     // Opens up a preview for one of the products.
     // Its parameters are an index from the hash and the products object.
-    function renderSingleProductPage(index, data){
+    function renderSingleProductPage(index, data) {
 
         var page = $('.single-product'),
             container = $('.preview-large');
 
         // Find the wanted product by iterating the data object and searching for the chosen index.
-        if(data.length){
+        if (data.length) {
             data.forEach(function (item) {
-                if(item.id == index){
+                if (item.id == index) {
                     // Populate '.preview-large' with the chosen product's data.
                     container.find('h3').text(item.name);
                     container.find('img').attr('src', item.image.large);
@@ -264,10 +311,10 @@ $(function () {
     // Find and render the filtered data results. Arguments are:
     // filters - our global variable - the object with arrays about what we are searching for.
     // products - an object with the full products list (from products.json).
-    function renderFilterResults(filters, products){
+    function renderFilterResults(filters, products) {
 
         // This array contains all the possible filter criteria.
-        var criteria = ['categories','storage','os','camera'],
+        var criteria = ['categories', 'storage', 'os', 'camera', 'price','name'],
             results = [],
             isFiltered = false;
 
@@ -279,13 +326,13 @@ $(function () {
         criteria.forEach(function (c) {
 
             // Check if each of the possible filter criteria is actually in the filters object.
-            if(filters[c] && filters[c].length){
+            if (filters[c] && filters[c].length) {
 
 
                 // After we've filtered the products once, we want to keep filtering them.
                 // That's why we make the object we search in (products) to equal the one with the results.
                 // Then the results array is cleared, so it can be filled with the newly filtered data.
-                if(isFiltered){
+                if (isFiltered) {
                     products = results;
                     results = [];
                 }
@@ -298,31 +345,84 @@ $(function () {
                 filters[c].forEach(function (filter) {
 
                     // Iterate over the products.
-                    products.forEach(function (item){
+                    products.forEach(function (item) {
 
                         // If the product has the same specification value as the one in the filter
                         // push it inside the results array and mark the isFiltered flag true.
 
-                        if(typeof item.specs[c] == 'number'){
-                            if(item.specs[c] == filter){
+                        if (typeof item.specs[c] == 'number') {
+                            if (item.specs[c] == filter) {
                                 results.push(item);
                                 isFiltered = true;
                             }
                         }
 
-                        if(typeof item.specs[c] == 'string'){
-                            if(item.specs[c].toLowerCase().indexOf(filter) != -1){
+                        if (typeof item.specs[c] == 'string') {
+                            if (item.specs[c].toLowerCase().indexOf(filter) != -1) {
                                 results.push(item);
                                 isFiltered = true;
                             }
                         }
 
-                    });
+                        if (c == 'price') {
+                            if (filter == 1000) {
+                                if (item[c] <= filter) {
+                                    results.push(item);
+                                    isFiltered = true;
+
+                                }
+                            }
+
+                            else if (filter == 3000) {
+                                if (item[c] > 1000 && item[c] <= filter) {
+                                    results.push(item);
+                                    isFiltered = true;
+
+                                }
+                            }
+                            else if (filter == 6000) {
+                                    if (item[c] > 3000 && item[c] <= filter) {
+                                        results.push(item);
+                                        isFiltered = true;
+
+                                    }
+                                }
+
+                                else if (filter == 10000) {
+                                if (item[c] > 6000 && item[c] <= filter) {
+                                    results.push(item);
+                                    isFiltered = true;
+
+                                }
+                            }
+
+
+                                    else if (filter == 10001) {
+                                        if (item[c] > 10000 && item[c] <= filter) {
+                                            results.push(item);
+                                            isFiltered = true;
+
+                                        }
+                                    }
+                                }
+
+                            if (c == 'name') {
+                                if (item[c] == filter) {
+                                    results.push(item);
+                                    isFiltered = true;
+                                }
+                            }
+
+
+
+
+                        }
+                        );
 
                     // Here we can make the checkboxes representing the filters true,
                     // keeping the public up to date.
-                    if(c && filter){
-                        $('input[name='+c+'][value='+filter+']').prop('checked',true);
+                    if (c && filter) {
+                        $('input[name=' + c + '][value=' + filter + ']').prop('checked', true);
                     }
                 });
             }
@@ -336,20 +436,20 @@ $(function () {
 
 
     // Shows the error page.
-    function renderErrorPage(){
+    function renderErrorPage() {
         var page = $('.error');
         page.addClass('visible');
     }
 
     // Get the filters object, turn it into a string and write it into the hash.
-    function createQueryHash(filters){
+    function createQueryHash(filters) {
 
         // Here we check if filters isn't empty.
-        if(!$.isEmptyObject(filters)){
+        if (!$.isEmptyObject(filters)) {
             // Stringify the object via JSON.stringify and write it after the '#filter' keyword.
             window.location.hash = '#filter/' + JSON.stringify(filters);
         }
-        else{
+        else {
             // If it's empty change the hash to '#' (the homepage).
             window.location.hash = '#';
         }
